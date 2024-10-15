801ca2ed4bbfab697f2c6cfa3996676b3bba2da7
Veronica Araque Franco <veronica.araque16@gmail.com>
2024-10-14 23:26:11 -0300

    stable version, integrating tailwind css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./db.js');
 const PORT = require('./config.js');
 
 
-conn.sync({ force: true })
+conn.sync({ force: false })
     .then(() => {
         server.listen(PORT, () => console.log(`Listening on the port: ${PORT}`));
     });
